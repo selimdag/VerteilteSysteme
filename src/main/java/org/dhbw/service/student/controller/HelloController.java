b5abb261f3b042d188f308d411a82e22ce297041
Selami Dagtasoglu <sdagtasoglu@gmail.com>
2020-04-21 10:19:24 +0200

    Initial Commit

---
+++ src/main/java/org/dhbw/service/student/controller/HelloController.java
@@ -0,0 +1,17 @@
+package org.dhbw.service.student.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HelloController
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hallo Welt!";
+    }
+
+}
